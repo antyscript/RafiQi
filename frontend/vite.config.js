e327317ff1de024098f88424b29b9eaceac199aa
antyscript <antyscript@gmail.com>
2025-09-10 16:04:19 +0100

    fonts on ofline

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
 		react(),
 		VitePWA({
 			registerType: "autoUpdate",
+			workbox: {
+				globPatterns: ["**/*.{js,css,html,ico,png,svg,ttf,woff,woff2}"]
+			},
 			includeAssets: [
 				"favicon.ico",
 				"apple-touch-icon.png",
